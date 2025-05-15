af6463c83bb7126c83963febef94bf9f2dc712bf
Tess99854 <tesnimesb@gmail.com>
2025-05-15 15:29:31 +0100

    update urls

---
+++ backend/sharing/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('',      views.CreateShareView.as_view(),   name='create_share'),
+    # path('',      views.DeleteShareView.as_view(),   name='delete_share'),
+    # path('',      views.ListSharesView.as_view(),    name='list_shares'),
+]
---
+++ backend/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('login/',  views.LoginView.as_view(),   name='login'),
+    # path('refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
+    # path('user/',   views.UserDetailView.as_view(), name='user_detail'),
+    # path('logout/', views.LogoutView.as_view(), name='logout'),
+]
