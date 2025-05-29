dbd810ca5c47e39e84344d5cd106049fc33b3708
Mahmoud Mohammed Ali <ibn.el4ai5@gmail.com>
2025-05-29 22:52:22 +0100

    public files shared and previewed

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from public.views import PublicPageServeView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "published/<str:username>/<str:filename>/",
+        PublicPageServeView.as_view(),
+        name="public-page-serve",
+    ),
     # Authentication
     path("api/auth/", include("authentication.urls")),
     # Files
